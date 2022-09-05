5ce2bb893f182a5ff2f216a48fc12f49ffd4ee55
anton0xf <anton0xf@gmail.com>
2022-09-05 04:10:24 +0300

    youtube bb: get first page

--- youtube/parse_playlist.clj
+++ youtube/parse_playlist.clj
@@ -17,41 +17,50 @@
      ~@body
      (flush)))
 
-(defn try-get-arg []
-  (when-not *command-line-args*
-    (stderr (printf "Usage: ./%s playlist_id%n" (script-name)))
-    (System/exit 1))
-  (first *command-line-args*))
-
-(defn get-playlist-id []
-  (let [id (try-get-arg)]
-    (stderr (println "playlist id:" id))
-    id))
-
-(defn get-api-key [] "")
+(defn get-api-key []
+  (or (System/getenv "YOUTUBE_API_KEY")
+      (slurp ".api-key")))
 
 (defn get-playlist-items-page [id]
-  (try (curl/get playlist-items-base-url
-                 {:query-params
-                  {"key" (get-api-key)
-                   "playlistId" id
-                   "part" "id,snippet"
-                   "maxResults" 50}})
+  (try (-> playlist-items-base-url
+           (curl/get
+            {:query-params
+             {"key" (get-api-key)
+              "playlistId" id
+              "part" "id,snippet"
+              "maxResults" 50}})
+           :body
+           (json/parse-string true))
        (catch Exception ex
          (stderr (println (ex-message ex))
                  (pprint (-> (ex-data ex)
                              :body
                              (json/parse-string true))))
-         ;; (System/exit 2)
-         )))
+         nil)))
+
+(defn get-video-info [snippet]
+  {:title (:title snippet)
+   :video-id (-> snippet :resourceId :videoId)})
 
 (defn get-playlist-items [id]
-  (let [page (get-playlist-items-page id)]
-    page))
+  (let [page (get-playlist-items-page id)
+        next-page-token (:nextPageToken page)
+        items (:items page)
+        snippets (map :snippet items)
+        res (map get-video-info snippets)]
+    (stderr (println "next page token:" next-page-token))
+    res))
+
+(defn playlist-to-tsv [id]
+  (stderr (println "playlist id:" id))
+  (doseq [item (get-playlist-items id)]
+    (printf "%s\t%s\n" (:video-id item) (:title item)))
+  (flush))
 
 ;; https://github.com/babashka/babashka/blob/master/examples/htmx_todoapp.clj#L235
 (when (= *file* (System/getProperty "babashka.file"))
-  (let [id (get-playlist-id)
-        items (get-playlist-items id)]
-    items))
+  (if-let [playlist-id (first *command-line-args*)]
+    (playlist-to-tsv playlist-id)
+    (do (stderr (printf "Usage: ./%s playlist_id%n" (script-name)))
+        (System/exit 1))))
 
